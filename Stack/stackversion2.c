6d29605236a2e1765ce5712affa6598e169c2761
Lye71 <ianree71@gmail.com>
2026-02-16 13:40:58 +0800

    added c file for stack version 1 and version 2

---
+++ Stack/stackversion2.c
@@ -0,0 +1,29 @@
+#define MAX 10
+
+typedef struct{
+    int items[MAX];
+    int top;
+}Stack;
+
+void init(Stack **s);
+void push(Stack* s, int value);
+void pop(Stack* s);
+
+void init(Stack **s){
+    (*s)->top = MAX;
+}
+
+void push(Stack* s, int value){
+    if(isFull(s)){
+        return;
+    }
+    s->top--;
+    s->items[s->top] = value;
+}
+
+void pop(Stack* s){
+    if(isEmpty(s)){
+        return;
+    }
+    s->top++;
+}
